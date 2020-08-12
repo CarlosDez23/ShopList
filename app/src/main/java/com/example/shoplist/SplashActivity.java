35e9248e662fbe5de4f6339aac19ef8470cfb891
CarlosDez23 <cgonzalezd10@hotmail.com>
2020-08-12 13:09:10 +0200

    Funcionalidad splash screen

--- app/src/main/java/com/example/shoplist/SplashActivity.java
+++ app/src/main/java/com/example/shoplist/SplashActivity.java
@@ -2,7 +2,10 @@ package com.example.shoplist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
