685c6a7a22dc63f5556dbd4450643a3968c8481d
CarlosDez23 <cgonzalezd10@hotmail.com>
2020-08-12 13:28:46 +0200

    Mas ajustes de estructura

---
+++ app/src/main/java/com/example/shoplist/ui/catalogo/CatalogoFragment.java
@@ -0,0 +1,21 @@
+package com.example.shoplist.ui.catalogo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.shoplist.R;
+
+public class CatalogoFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        return root;
+    }
+}
--- app/src/main/java/com/example/shoplist/ui/compras/ComprasFragment.java
+++ app/src/main/java/com/example/shoplist/ui/compras/ComprasFragment.java
@@ -1,4 +1,4 @@
-package com.example.shoplist.ui.home;
+package com.example.shoplist.ui.compras;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,7 +10,7 @@ import androidx.fragment.app.Fragment;
 
 import com.example.shoplist.R;
 
-public class HomeFragment extends Fragment {
+public class ComprasFragment extends Fragment {
 
 
 
